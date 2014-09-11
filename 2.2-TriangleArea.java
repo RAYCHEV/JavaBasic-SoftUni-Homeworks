b764c13fcbdad151cbd5f755bd080e423c101e5f
RAYCHEV <nikolay.g.raychev@mail.bg>
2014-09-11 05:25:30 +0300

    Create 2.2-TriangleArea.java

---
+++ 2.2-TriangleArea.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class _2_TriangleArea_int {
+	
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		int Ax = scan.nextInt();
+		int Ay = scan.nextInt();
+		int Bx = scan.nextInt();
+		int By = scan.nextInt();
+		int Cx = scan.nextInt();
+		int Cy = scan.nextInt();
+	
+		int area = (Ax*(By - Cy) + Bx*(Cy - Ay) + Cx*(Ay - By))/2;
+		area = Math.abs(area);
+		System.out.println(area);
+	}
+}
