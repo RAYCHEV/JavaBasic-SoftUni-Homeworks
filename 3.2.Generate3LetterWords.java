02154602cc70a54984a2d84fd4ab11ae6713f493
RAYCHEV <nikolay.g.raychev@mail.bg>
2014-09-13 06:24:00 +0300

    Update 3.2.Generate3LetterWords.java

--- 3.2.Generate3LetterWords.java
+++ 3.2.Generate3LetterWords.java
@@ -23,6 +23,7 @@ public class Generate3LetterWords {
 
 /*
 Write a program to generate and print all 3-letter words consisting of given set of characters.
-For example if we have the characters 'a' and 'b', all possible words will be "aaa", "aab", "aba", "abb", "baa", "bab", "bba" and "bbb". The input characters are given as string at the first line of the input. 
+For example if we have the characters 'a' and 'b', all possible words will be "aaa", "aab", "aba",
+"abb", "baa", "bab", "bba" and "bbb". The input characters are given as string at the first line of the input. 
 Input characters are unique (there are no repeating characters).
 */
